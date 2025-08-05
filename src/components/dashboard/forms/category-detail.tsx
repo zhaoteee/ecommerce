5e39894a06cf1616a8b6cda05d8c4e1e6b70c966
zhaote <zhaote555@gmail.com>
2025-08-05 16:14:58 +0800

    add basic form

--- src/components/dashboard/forms/category-detail.tsx
+++ src/components/dashboard/forms/category-detail.tsx
@@ -1,7 +1,147 @@
+"use client";
+import { AlertDialog } from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { Category } from "@/generated/prisma";
+import { CategoryFormSchema } from "@/lib/schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 const CategoryDetail = ({ data }: { data?: Category }) => {
-  return <div></div>;
+  const form = useForm<z.infer<typeof CategoryFormSchema>>({
+    mode: "onChange",
+    resolver: zodResolver(CategoryFormSchema),
+    defaultValues: {
+      featured: data?.featured || false,
+      name: data?.name || "",
+      url: data?.url || "",
+    },
+  });
+  useEffect(() => {
+    if (data) {
+      form.reset({
+        featured: data.featured,
+        name: data.name || "",
+        url: data.url || "",
+        image: [{ url: data.image }],
+      });
+    }
+  }, [data, form]);
+  const handleSubmit = (values: z.infer<typeof CategoryFormSchema>) => {
+    console.log(values);
+  };
+  const isLoading = form.formState.isSubmitting;
+
+  return (
+    <AlertDialog>
+      <Card>
+        <CardHeader>
+          <CardTitle>Category Information</CardTitle>
+          <CardDescription>
+            {data?.id
+              ? `Update ${data.name} category information`
+              : "Let's create a category."}
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit((v) => {
+                console.log("表单错误信息:", form.formState.errors);
+                console.log("表单值:", v);
+                handleSubmit(v);
+              })}
+              className="space-y-4"
+            >
+              <FormField
+                disabled={isLoading}
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel>Category name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Name" {...field}></Input>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              ></FormField>
+              <FormField
+                disabled={isLoading}
+                control={form.control}
+                name="url"
+                render={({ field }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel>Url</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Url" {...field}></Input>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              ></FormField>
+              <FormField
+                disabled={isLoading}
+                control={form.control}
+                name="featured"
+                render={({ field }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel>Featured</FormLabel>
+                    <FormControl>
+                      <Checkbox
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      ></Checkbox>
+                    </FormControl>
+                    <div className="space-y-1 leading-none">
+                      <FormDescription>
+                        This category will appear on the home page
+                      </FormDescription>
+                    </div>
+                  </FormItem>
+                )}
+              ></FormField>
+              <Button
+                type="submit"
+                disabled={isLoading}
+                onClick={() => {
+                  console.log("当前表单值:", form.getValues());
+                  console.log("当前表单错误:", form.formState.errors);
+                  console.log("表单是否有效:", form.formState.isValid);
+                }}
+              >
+                {isLoading
+                  ? "loading..."
+                  : data?.id
+                  ? "Save category information"
+                  : "Create category"}
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </AlertDialog>
+  );
 };
 
 export default CategoryDetail;
---
+++ src/lib/schemas.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+
+export const CategoryFormSchema = z.object({
+  name: z
+    .string()
+    .min(2, "Category name must be at least 2 characters long.")
+    .max(50, "Category name cannot be exceed 50 characters.")
+    .regex(
+      /^[a-zA-Z0-9\s]+$/,
+      "Only letters, numbers, and spaces are allowed in the category name."
+    ),
+  image: z
+    .object({ url: z.string() })
+    .array()
+    .length(1, "Choose only one category image"),
+  url: z
+    .string()
+    .min(2, "Category url must be at least 2 characters long.")
+    .max(50, "Category name cannot be exceed 50 characters.")
+    .regex(
+      /^(?!.*(?:[-_]){2,})[a-zA-Z0-9_-]+$/,
+      "Only letters, numbers, and spaces are allowed in the category url."
+    ),
+  featured: z.boolean(),
+});
